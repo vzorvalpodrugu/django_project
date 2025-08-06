1e9245a105e50dcab5a07344caad9ab1487d4394
vzorvalpodrugu <Starkovqw8904@gmail.com>
2025-08-06 21:41:20 +0300

    hw23: С помощью shell_plus добавлены данные. Ознакомиться можно на скринах

--- barbershop/settings.py
+++ barbershop/settings.py
@@ -24,7 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django-extensions',
+    'django_extensions',
     'core'
 ]
 
